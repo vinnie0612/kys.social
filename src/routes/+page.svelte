c13611ac959d468909e598869a53b59511272e1b
Tamás Vince Kornél <tamas.vince.kornel@petrik.hu>
2023-10-03 01:09:29 +0200

    simple home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  const letters = 'abcdefghijklmnopqrstuvxyz.';
+
+  let interval: any = null;
+  let text: string = 'kys.social';
+  let oText: string = text;
+
+  function handleHover(): void {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      text = text
+        .split('')
+        .map((letter, index) => {
+          if (index < iteration) {
+            return oText[index];
+          }
+
+          return letters[Math.floor(Math.random() * text.length)];
+        })
+        .join('');
+
+      if (iteration >= oText.length) {
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 3;
+    }, 10);
+  }
+</script>
+
+<main class="bg-stone-900 text-white h-full">
+  <div class="flex items-center justify-center h-full flex-col">
+    <h1 class="text-6xl" on:mouseover={handleHover} on:focus>{text}</h1>
+    <h2 class="text-3xl">pull the trigger</h2>
+  </div>
+</main>
